dc0e94e21fc7f94b8360586da9e2c9b67d03a06b
Alejandro Fuentes Cleto <fuentescletoalejandro@gmail.com>
2021-07-28 17:48:35 -0500

    Install Swiper

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Swiper, SwiperSlide } from "swiper/react"
 
 function App() {
+  const swiperSettings = {
+    spaceBetween: 50,
+    slidesPerView: 1,
+    onSlideChange: () => console.log("Slide change"),
+    onSwiper: (swiper) => console.log(swiper)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="">
+      <Swiper {...swiperSettings}>
+        <SwiperSlide>
+          1
+        </SwiperSlide>
+        <SwiperSlide>
+          2
+        </SwiperSlide>
+        <SwiperSlide>
+          3
+        </SwiperSlide>
+        <SwiperSlide>
+          4
+        </SwiperSlide>
+      </Swiper>
+      <header className="">
       </header>
     </div>
   );
